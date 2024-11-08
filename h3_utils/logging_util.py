d2fdb2a4be9b334ab732d33ee28f842c3485510d
Aavato-c <aavatoc@gmail.com>
2024-11-08 20:21:46 +0200

    Enhance LoggingUtil constructor to include dont_overwrite_progress_bar parameter for flexible stream handler management

--- h3_utils/logging_util.py
+++ h3_utils/logging_util.py
@@ -8,9 +8,8 @@ from httpx import stream
 if not os.path.exists('logs'):
     os.makedirs('logs')
     
-
 class LoggingUtil:
-    def __init__(self, name: str = None):
+    def __init__(self, name: str = None, dont_overwrite_progress_bar: bool = True):
         if not name:
             random_chars = ''.join(random.choices(string.ascii_uppercase + string.digits, k=3))
             self.logger = logging.getLogger(random_chars)
@@ -21,11 +20,15 @@ class LoggingUtil:
         file_handler = logging.FileHandler('logs/combined.log')
         file_handler.setLevel(logging.DEBUG)
         file_handler.setFormatter(formatter)
-        stream_handler = logging.StreamHandler()
-        stream_handler.setFormatter(formatter)
-        stream_handler.setLevel(logging.DEBUG)
-        self.logger.addHandler(stream_handler)
         self.logger.addHandler(file_handler)
+
+        if dont_overwrite_progress_bar != True:
+            stream_handler = logging.StreamHandler()
+            stream_handler.setFormatter(formatter)
+            stream_handler.setLevel(logging.DEBUG)
+            self.logger.addHandler(stream_handler)
+
+
 
     def get_logger(self):
         return self.logger
