82c975bdcb6f751f965180ce1c09a909e84de824
Aavato-c <80039165+Aavato-c@users.noreply.github.com>
2024-11-11 21:49:27 +0200

    Refactor: Cleanup main.py from comments and imports

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from h3_utils.launch import prepare_environment
+from h3_utils.launch.launch import prepare_environment
 prepare_environment()
 
 from modules.async_worker import ImageTaskProcessor
-import ldm_patched.modules.model_management as model_management
-from h3_utils.config import ImageGenerationObject, EnhanceMaskCtrls
-from unavoided_global_hell.global_model_management import global_model_management
+import ldm_patched.modules.model_management
+from h3_utils.config import ImageGenerationObject
+from unavoided_globals.global_model_management import global_model_management
 
 import time
 
@@ -21,22 +21,6 @@ def generate_image(task: ImageGenerationObject):
 
         time.sleep(0.01)
         imgProcessor.process_all_tasks()
-        """ if len(imgProcessor.yields) > 0:
-            flag, product = imgProcessor.yields.pop(0)
-            if flag == 'preview':
-                # help bad internet connection by skipping duplicated preview
-                if len(imgProcessor.yields) > 0:  # if we have the next item
-                    if imgProcessor.yields[0][0] == 'preview':   # if the next item is also a preview
-                        # print('Skipped one preview for better internet connection.')
-                        continue
-
-                percentage, title, image = product
-                print(f'{percentage:.2f}%: {title}')
-                ...
-            if flag == "results":
-                ...
-            if flag == 'finish':
-                finished = True """
         print('Image generation finished.')
         ...
 
@@ -51,5 +35,7 @@ if __name__ == '__main__':
     image_params = ImageGenerationObject(
         prompt="A car without headlights a banana"
         )
+    
     generate_image(image_params)
+    
     print('Done fom main.py')
